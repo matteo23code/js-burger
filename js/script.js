f9d46919c85ec9808cb4856e91143e975d285210
Matteo Basile <matteo.basile56@gmail.com>
2020-12-21 16:43:54 +0100

    prova sconto

--- js/script.js
+++ js/script.js
@@ -19,4 +19,14 @@ document.getElementById("button-ingredient").addEventListener("click", function(
   // popolo elemento #total con il totale del mio ordine
   totalHtmlElement.getElementsByTagName('span')[0].innerText = total;
 
+  var discount = document.getElementsByClassName('discount');
+
+  var couponCode = ['001','002','003','004'];
+
+  if (couponCode.includes(discount)) {
+    var sconto = (total/100 ) * 20;
+    costs = total - sconto;
+  }else {
+    alert('Inserisci codice sconto valido!');
+  }
 });
